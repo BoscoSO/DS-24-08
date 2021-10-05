0e4fef0f395921a703495d76de8231956bfff3cc
BoscoSO <63974401+BoscoSO@users.noreply.github.com>
2021-10-05 13:03:39 +0200

    end

--- DS-24-08-B1/src/e1/StringCount.java
+++ DS-24-08-B1/src/e1/StringCount.java
@@ -1,5 +1,7 @@
 package e1;
 
+import java.util.Locale;
+
 public class StringCount {
     /**
      * Counts the number of words in a given String .
@@ -8,25 +10,58 @@ public class StringCount {
      * @param text String with the words
      * @return Number of words in the String or zero if it is null
      */
-    public static int countWords ( String text ) { return text.split(" ").length;
+    public static int countWords(String text) {
+        if (text == null) return 0;
+        int cont = 0;
+
+        for (String c : text.split(" ")) {
+            if (c.length() != 0) cont++;
         }
+        return cont;
+    }
+
     /**
      * Counts the number of times the given character appears in the String .
      * Accented characters are considered different characters .
+     *
      * @param text String with the characters
-     * @param c the character to be found
+     * @param c    the character to be found
      * @return Number of times the character appears in the String or zero if null
      */
-    public static int countChar ( String text , char c) { /* ... */ return 0;}
+    public static int countChar(String text, char c) {
+        if (text == null) return 0;
+        int cont = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == c) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
     /**
      * Counts the number of times the given character appears in the String .
      * The case is ignored so an ’a’ is equal to an ’A ’.
      * Accented characters are considered different characters .
+     *
      * @param text String with the characters
-     * @param c the character to be found
+     * @param c    the character to be found
      * @return Number of times the character appears in the String or zero if null
      */
-    public static int countCharIgnoringCase ( String text , char c ) { /* ... */ return 0;}
+    public static int countCharIgnoringCase(String text, char c) {
+        if (text == null) return 0;
+        int cont = 0;
+        text = text.toLowerCase();
+        c = Character.toLowerCase(c);
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == c) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
     /**
      * Checks if a password is safe according to the following rules :
      * - Has at least 8 characters
@@ -34,10 +69,33 @@ public class StringCount {
      * - Has a lowercase character
      * - Has a digit
      * - Has a special character among these : ’? ’, ’@ ’, ’#’, ’$ ’, ’. ’ and ’,’
+     *
      * @param password The password , we assume it is not null .
      * @return true if the password is safe , false otherwise
      */
-    public static boolean isPasswordSafe ( String password ) {
+    public static boolean isPasswordSafe(String password) {
+        char ch;
+        if (password.length() >= 8) {
+            boolean upperCaseFlag = false;
+            boolean lowerCaseFlag = false;
+            boolean digitFlag = false;
+            boolean specialFlag = false;
 
-        /* ... */ return false;}
+            for (int i = 0; i < password.length(); i++) {
+                ch = password.charAt(i);
+                if (Character.isDigit(ch)) {
+                    digitFlag = true;
+                } else if (Character.isUpperCase(ch)) {
+                    upperCaseFlag = true;
+                } else if (Character.isLowerCase(ch)) {
+                    lowerCaseFlag = true;
+                } else if (ch=='?' ||ch=='@' ||ch=='#' ||ch=='$' ||ch=='.' ||ch==',') {
+                    specialFlag = true;
+                }
+                if (digitFlag && upperCaseFlag && lowerCaseFlag && specialFlag)
+                    return true;
+            }
+        }
+        return false;
+    }
 }
